6bf27705f71f9266bb2b2698e119c25e2fcb4ad0
nicesoft-labs <info@ncsgp.ru>
2025-06-14 11:35:16 +0300

    Update gost-helper.h

--- g10/gost-helper.h
+++ g10/gost-helper.h
@@ -1,11 +1,12 @@
 #ifndef GNUPG_G10_GOST_HELPER_H
 #define GNUPG_G10_GOST_HELPER_H
 
-#include "gost-kdf.h"
-#include <gcrypt.h>
 #include <gpg-error.h>
+#include <gcrypt.h>
+#include "gost-kdf.h"
 
-gpg_error_t pk_gost_get_kdf_params (gcry_mpi_t *pkey,
-                                    gost_kdf_params_t **r_params);
+/* Извлечь из публичного ключа массив MPI параметры KDF/VKO */
+gpg_error_t pk_gost_get_kdf_params(gcry_mpi_t *pkey,
+                                   gost_kdf_params_t **r_params);
 
 #endif /* GNUPG_G10_GOST_HELPER_H */
